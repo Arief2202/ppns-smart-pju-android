18812962e20917cbdd35b8012387225e36e63779
Arief <arief.d2202@gmail.com>
2024-04-13 02:22:43 +0700

    delete unused var

--- lib/global_var.dart
+++ lib/global_var.dart
@@ -5,9 +5,4 @@ bool isLoggedIn = false;
 String endpoint = "192.168.100.59";
 
 const int httpTimeout = 1;
-String user_id = "";
-String user_name = "";
-String user_email = "";
-String user_password = "";
-String user_password_encrypted = "";
 
